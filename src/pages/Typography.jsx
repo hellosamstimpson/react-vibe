2daf31c860a1cd2389a79685ef52649244776c2d
Sam Stimpson <hello+code@samstimpson.com>
2025-04-01 17:41:34 +0100

    Added header concept trying to copy basecamp layout as a goal for learning grid...

--- src/pages/Typography.jsx
+++ src/pages/Typography.jsx
@@ -3,7 +3,14 @@ import TypographyTest from "../components/TypographyTest"
 function Typography() {
     return (
         <div>
-            <TypographyTest />
+            <div className="layout">
+                <header className="layout__header">
+                    <h1>Website Header</h1>
+                </header>
+            </div>
+            <main>
+                <TypographyTest />
+            </main>
         </div>
     )
 }
